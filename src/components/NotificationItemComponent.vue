03b193ff2039f1f30c10762a76097144772615c8
Osman Nuri ERDOGAN <osmerd04@gmail.com>
2022-09-17 18:45:09 +0300

    Project started. Layout coded.

--- src/components/NotificationItemComponent.vue
+++ src/components/NotificationItemComponent.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
 li.notification__item
-  img.notification__image(:src="notification.user.image_url")
-  div
+  //img.notification__image(:src="notification.user.image_url")
+  img.notification__image(src="../assets/images/avatar-anna-kim.webp")
+  div.notification__content
+    p #[strong.notification__content__user-name {{ notification.user.name }}] #[span.notification__content__prefix {{ notification.prefix }}] #[span.notification__content__title {{ notification.title }}]
+    span.notification__content__time {{notification.time}}
 </template>
 <script setup>
 const props = defineProps({
@@ -17,6 +20,36 @@ const props = defineProps({
   &__item {
     font-size: $font-size-paragraph;
     list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 2.4rem;
+  }
+  &__image {
+    width: 10%;
+    height: auto;
+  }
+  &__content {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-direction: column;
+    gap: 0.15rem;
+    &__prefix {
+      /* color: $color-secondary-grayish-blue; */
+      color: $color-secondary-dark-grayish-blue;
+      font-weight: $font-weight-medium;
+    }
+    &__title {
+      font-weight: $font-weight-extra-bold;
+      color: $color-secondary-dark-grayish-blue;
+    }
+    &__user-name {
+      font-weight: $font-weight-extra-bold;
+    }
+    &__time {
+      color: $color-secondary-dark-grayish-blue;
+    }
   }
 }
 </style>
--- src/components/NotificationListComponent.vue
+++ src/components/NotificationListComponent.vue
@@ -1,19 +1,22 @@
 <template lang="pug">
 ul.notification__list
-  NotificationItemComponent(v-for="i in 5" :key="i" :notification="notification.value") {{ i }}
+  NotificationItemComponent(v-for="i in 5" :key="i" :notification="notification") {{ i }}
 </template>
 <script setup>
-const notification = {
+import NotificationItemComponent from "./NotificationItemComponent.vue";
+import { ref } from "vue";
+const notification = ref({
   user: {
     name: "Osman Nuri Erdogan",
-    image_url: "https://google.com",
+    image_url:
+      "https://media.istockphoto.com/photos/trangthailand-october-24-2019-customer-lay-down-new-macbook-pro-2019-picture-id1183532123?s=612x612",
   },
   status: "unread",
-  prefix: "reacted to your recent posts!",
+  prefix: "reacted to your recent posts",
   title: "My first tournament today!",
-};
-import NotificationItemComponent from "./NotificationItemComponent.vue";
-return { notification };
+  time: "1 day ago",
+});
+// return { notification };
 </script>
 <style lang="scss">
 @import "../assets/sass/variables";
